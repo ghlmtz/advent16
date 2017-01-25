f108e7f832175d981ebe9520ceb601430f64e506
ghlmtz <devilishgiant@gmail.com>
2017-01-24 22:12:10 -0500

    Part two of day 4 done

--- 04/04.py
+++ 04/04.py
@@ -11,3 +11,16 @@ with open('04.in') as input:
 		if chksum == "".join(s_name[:5]):
 			ids += int(re.search('\d+',line).group(0))
 print ids
+
+### Part two ###
+import string
+
+s = "abcdefghijklmnopqrstuvwxyz"
+with open('04.in') as input:
+	for line in input:
+		names = re.split('-',line)
+		rot = int(re.search('\d+',line).group(0)) % 26
+		table = string.maketrans(s,s[rot:]+s[:rot])
+		for name in names:
+			if name.translate(table).startswith('north'):
+				print int(re.search('\d+',line).group(0))
