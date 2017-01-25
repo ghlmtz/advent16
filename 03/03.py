54da54b3321c4eb67224eac49a15f0ca4499071b
ghlmtz <devilishgiant@gmail.com>
2017-01-24 21:07:32 -0500

    Day 3 is completed

--- 03/03.py
+++ 03/03.py
@@ -1,9 +1,19 @@
 ### Part one ###
 count = 0
-
 with open('03.in') as input:
 	for line in input:
 		nums = [int(c) for c in line.split()]
+		if max(nums) < sum(nums)/2.0:
+			count += 1
+print count
+
+### Part two ###
+count = 0
+with open('03.in') as input:
+	lines = input.readlines()
+for x in range(len(lines)/3):
+	for i in range(3):
+		nums = [int(lines[3*x].split()[i]), int(lines[3*x+1].split()[i]), int(lines[3*x+2].split()[i])]
 		if max(nums) < sum(nums)/2.0:
 			count += 1
 print count
