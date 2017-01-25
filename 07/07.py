e491afade1b08ee81e1f79a08b1aed4fc82d32a1
ghlmtz <devilishgiant@gmail.com>
2017-01-25 00:11:34 -0500

    First part is done

--- 07/07.py
+++ 07/07.py
@@ -2,13 +2,37 @@
 import re
 
 def abba(s):
-	if s[0] == s[3] and s[1] == s[2] and s[0] != s[1]:
-		return True
-	else:
-		return False
+	return ( s[0] == s[3] and s[1] == s[2] and s[0] != s[1] )
 
 with open('07.in') as input:
 	lines = input.readlines()
-for addr in lines:
-	
-
+count = 0
+for N,addr in enumerate(lines):
+	blk = 0
+	yes = 0
+	for i in range(len(addr)-4):
+		substr = addr[i:i+4]
+		if blk == 1:
+			if substr.count('['):
+				continue
+			blk = 2
+		if blk == 2:
+			if substr.count(']'):
+				blk = 3
+				continue
+			if(abba(substr)):
+				yes = 0
+				break
+		if blk == 3:
+			if substr.count(']'):
+				continue
+			blk = 0
+		if blk == 0:
+			if substr.count('['):
+				blk = 1
+				continue
+			if(abba(substr)):
+				yes = 1
+	if yes == 1:
+		count += 1
+print count
