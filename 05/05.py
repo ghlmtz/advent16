4ff0996c6e51f3339b5a02b5db395b6c1472be18
ghlmtz <devilishgiant@gmail.com>
2017-01-24 22:49:45 -0500

    Password cracked: day 5 done

--- 05/05.py
+++ 05/05.py
@@ -13,4 +13,16 @@ while n > 0:
 		n -= 1
 	i += 1
 
-
+### Part two ###
+i = 0
+password = '________'
+while password.count('_'):
+	m = hashlib.md5()
+	m.update(input + str(i))
+	hex_str = binascii.hexlify(m.digest())
+	if hex_str.startswith('00000') and int(hex_str[5],16) < 8:
+		idx = int(hex_str[5])
+		if password[idx] == '_':
+			password = password[:idx] + hex_str[6] + password[idx+1:]
+	i += 1
+print password
